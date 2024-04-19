01d39887dc14919581096949a06b12f014c47ea3
Shigma <shigma10826@gmail.com>
2024-04-19 15:33:21 +0800

    feat(core): implement core

---
+++ packages/core/src/client.ts
@@ -0,0 +1,72 @@
+import { Context, Logger } from 'cordis'
+import { WebSocket } from './types'
+import { DataService } from './service'
+import { IncomingMessage } from 'node:http'
+
+const logger = new Logger('console')
+
+export function coerce(val: any) {
+  // resolve error when stack is undefined, e.g. axios error with status code 401
+  const { message, stack } = val instanceof Error && val.stack ? val : new Error(val as any)
+  const lines = stack.split('\n')
+  const index = lines.findIndex(line => line.endsWith(message))
+  return lines.slice(index).join('\n')
+}
+
+export class Client {
+  readonly id = Math.random().toString(36).slice(2)
+
+  constructor(readonly ctx: Context, public socket: WebSocket, public request?: IncomingMessage) {
+    socket.addEventListener('message', this.receive)
+    ctx.on('dispose', () => {
+      socket.removeEventListener('message', this.receive)
+    })
+    this.refresh()
+  }
+
+  send(payload: any) {
+    this.socket.send(JSON.stringify(payload))
+  }
+
+  receive = async (data: WebSocket.MessageEvent) => {
+    const { type, args, id } = JSON.parse(data.data.toString())
+    const listener = this.ctx.get('console').listeners[type]
+    if (!listener) {
+      logger.info('unknown message:', type, ...args)
+      return this.send({ type: 'response', body: { id, error: 'not implemented' } })
+    }
+
+    if (await this.ctx.serial('console/intercept', this, listener)) {
+      return this.send({ type: 'response', body: { id, error: 'unauthorized' } })
+    }
+
+    try {
+      const value = await listener.callback.call(this, ...args)
+      return this.send({ type: 'response', body: { id, value } })
+    } catch (e) {
+      logger.debug(e)
+      const error = coerce(e)
+      return this.send({ type: 'response', body: { id, error } })
+    }
+  }
+
+  refresh() {
+    Object.keys(this.ctx.root[Context.internal]).forEach(async (name) => {
+      if (!name.startsWith('console.')) return
+      const key = name.slice(8)
+      const service = this.ctx.get(name) as DataService
+      if (!service) return
+      if (await this.ctx.serial('console/intercept', this, service.options)) {
+        return this.send({ type: 'data', body: { key, value: null } })
+      }
+
+      try {
+        const value = await service.get(false, this)
+        if (!value) return
+        this.send({ type: 'data', body: { key, value } })
+      } catch (error) {
+        logger.warn(error)
+      }
+    })
+  }
+}
---
+++ packages/core/src/index.ts
@@ -0,0 +1,127 @@
+import { Context, Service } from 'cordis'
+import { Awaitable, Dict, valueMap } from 'cosmokit'
+import { DataService } from './service'
+import { Client } from './client'
+import { IncomingMessage } from 'node:http'
+import { Entry } from './entry'
+import { WebSocket } from './types'
+
+export * from './client'
+export * from './entry'
+export * from './service'
+
+type NestedServices = {
+  [K in keyof Console.Services as `console.${K}`]: Console.Services[K]
+}
+
+declare module 'cordis' {
+  interface Context extends NestedServices {
+    console: Console
+  }
+
+  interface Events {
+    'console/connection'(client: Client): void
+    'console/intercept'(client: Client, listener: DataService.Options): Awaitable<boolean>
+  }
+}
+
+export interface Console extends Console.Services {}
+
+export interface Listener extends DataService.Options {
+  callback(this: Client, ...args: any[]): Awaitable<any>
+}
+
+export interface EntryData {
+  files: string[]
+  paths?: string[]
+  data: () => any
+}
+
+export class EntryProvider extends DataService<Dict<EntryData>> {
+  static inject = []
+
+  constructor(ctx: Context) {
+    super(ctx, 'entry', { immediate: true })
+  }
+
+  async get(forced: boolean, client: Client) {
+    return this.ctx.get('console').get(client)
+  }
+}
+
+export abstract class Console extends Service {
+  static filter = false
+
+  private id = Math.random().toString(36).slice(2)
+
+  readonly entries: Dict<Entry> = Object.create(null)
+  readonly listeners: Dict<Listener> = Object.create(null)
+  readonly clients: Dict<Client> = Object.create(null)
+
+  constructor(public ctx: Context) {
+    super(ctx, 'console', true)
+    ctx.plugin(EntryProvider)
+    this.addListener('ping', () => 'pong')
+  }
+
+  protected accept(socket: WebSocket, request?: IncomingMessage) {
+    const client = new Client(this.ctx, socket, request)
+    socket.addEventListener('close', () => {
+      delete this.clients[client.id]
+      this.ctx.emit('console/connection', client)
+    })
+    this.clients[client.id] = client
+    this.ctx.emit('console/connection', client)
+  }
+
+  async get(client: Client) {
+    const result = valueMap(this.entries, ({ files, ctx, data }, key) => ({
+      files: this.resolveEntry(files, key),
+      paths: this.ctx.get('loader')?.paths(ctx.scope),
+      data: data?.(client),
+    }))
+    result['_id'] = this.id as any
+    return result
+  }
+
+  protected abstract resolveEntry(files: Entry.Files, key: string): string[]
+
+  addEntry<T>(files: Entry.Files, data?: () => T) {
+    return new Entry(this[Context.current], files, data)
+  }
+
+  addListener<K extends keyof Events>(event: K, callback: Events[K], options?: DataService.Options) {
+    this.listeners[event] = { callback, ...options }
+  }
+
+  async broadcast(type: string, body: any, options: DataService.Options = {}) {
+    const handles = Object.values(this.clients)
+    if (!handles.length) return
+    await Promise.all(Object.values(this.clients).map(async (client) => {
+      if (await this.ctx.serial('console/intercept', client, options)) return
+      const data = { type, body }
+      if (typeof body === 'function') data.body = await body(client)
+      client.socket.send(JSON.stringify(data))
+    }))
+  }
+
+  refresh<K extends keyof Console.Services>(type: K) {
+    return this.ctx.get(`console.${type}`)?.refresh()
+  }
+
+  patch<K extends keyof Console.Services>(type: K, value: Console.Services[K] extends DataService<infer T> ? T : never) {
+    return this.ctx.get(`console.${type}`)?.patch(value as any)
+  }
+}
+
+export interface Events {
+  'ping'(): string
+}
+
+export namespace Console {
+  export interface Services {
+    entry: EntryProvider
+  }
+}
+
+export default Console
